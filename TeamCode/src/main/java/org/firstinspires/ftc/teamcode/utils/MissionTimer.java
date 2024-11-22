0704b4fb8f225204eda4cc2faea772c21ae6118c
Steve-B <Steve-B@users.noreply.github.com>
2024-11-22 10:37:14 -0500

    Update MissionTimer.java
    Added heading comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MissionTimer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MissionTimer.java
@@ -3,6 +3,18 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.MILLISECONDS;
 
+/**
+ * This class uses an ElapsedTime to create a mission timer.
+ *
+ * PUBLIC METHODS:
+ *     MissionTimer() - constructor for instantiating a mission timer
+ *     void init() - initializes the mission timer
+ *     double getTimeMS() - returns current mission timer in milliseconds
+ *
+ * VERSION   DATE     WHO  DETAIL
+ * 00.01.00  11Nov24  SEB  Initial release
+ *
+ */
 public class MissionTimer {
 
     // Declare timer
